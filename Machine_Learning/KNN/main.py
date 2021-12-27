bcd0f51b129ef67f743fb1694e0ab1c653ebe738
BigSmoke908 <74255728+BigSmoke908@users.noreply.github.com>
2021-12-28 00:46:54 +0100

    Abstand kann fertig berechnet werden
    Problem welches noch besteht:
    wie findet man heraus, welcher Punkt der ist, der am dichtesten liegt?

--- Machine_Learning/KNN/main.py
+++ Machine_Learning/KNN/main.py
@@ -1,17 +1,25 @@
+from math import sqrt as wurzel
+from sortedcontainers import SortedDict
+
 alles = []
 a = []
 b = []
 c = []
 z = []
 
-x = 0
+ta = []
+tb = []
+tc = []
+tz = []
 
+x = 3 # welcher Punkt in den unbekannten Daten bestimmt werden soll
+
+
+# alle Daten werden sortiert
 with open('bekannt.txt') as f:
     alles = f.read()
     f.close()
-
     alles = alles.split()
-
 for i in range(len(alles)):
     alles[i] = int(alles[i])
 
@@ -27,16 +35,47 @@ for i in range(len(alles)):
         c.append(alles[i])
     else:
         z.append(alles[i])
-
-
 while len(alles) != 0:
     alles.remove(alles[0])
-
 # jetzt sind alle Daten in a, b, c, z aufgeteilt und damit verarbeitbar
 
-if x != 0:
-    x = x + ((x - 1) * 4)
+
+# die Testteile werden sortiert
+with open('unbekannt.txt') as f:
+    alles = f.read()
+    f.close()
+    alles = alles.split()
+for i in range(len(alles)):
+    alles[i] = int(alles[i])
+
+# jetzt sind alle Daten unsortiert in alles
+
+for i in range(len(alles)):
+    buffer = i % 4
+    if buffer == 0:
+        ta.append(alles[i])
+    elif buffer == 1:
+        tb.append(alles[i])
+    elif buffer == 2:
+        tc.append(alles[i])
+    else:
+        tz.append(alles[i])
+while len(alles) != 0:
+    alles.remove(alles[0])
+# jetzt sind die Punkte die geprüft werden sollen fertig sortiert in ta, tb, tc, tz
 
 
-#punkt = a[x], b[x], c[x], z[x]
-#print(punkt)
+distanz = {}
+# speichert den Abstand zwischen einem gegebenen Punkt (der der geprüft)
+# und allen Punkten aus den Trainingsdaten (Postion = Nummer von Punkt)
+# der Key ist Abstand und der Wert ist der Wert von dem Punkt
+
+for i in range(len(a)):
+    buffer = wurzel(((a[i] - ta[x]) ** 2) + ((b[i] - tb[x]) ** 2) + ((c[i] - tc[x]) ** 2))
+    print(buffer)
+    distanz[buffer] = {z[i]}
+
+distanz2 = SortedDict(distanz)
+
+print(distanz)
+print(distanz2)
