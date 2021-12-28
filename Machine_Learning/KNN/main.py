bd7cbe5c8c76a9cede2bbe488b7f9814fb6885f8
BigSmoke908 <74255728+BigSmoke908@users.noreply.github.com>
2021-12-28 11:35:00 +0100

    Finished Version
    - funktioniert relativ okay, braucht mehr Daten um bessere Ergebnisse zu liefern
    - befindet sich jetzt in einer Funktion, damit man es leichter nutzen kann

--- Machine_Learning/KNN/main.py
+++ Machine_Learning/KNN/main.py
@@ -1,81 +1,95 @@
 from math import sqrt as wurzel
-from sortedcontainers import SortedDict
 
-alles = []
-a = []
-b = []
-c = []
-z = []
+def knn_gewichtet(x):
+    global a, b, c, z, ta, tb, tc, tz, result
+    a = []
+    b = []
+    c = []
+    z = []
 
-ta = []
-tb = []
-tc = []
-tz = []
+    ta = []
+    tb = []
+    tc = []
+    tz = []
 
-x = 3 # welcher Punkt in den unbekannten Daten bestimmt werden soll
+    result = 0
 
 
-# alle Daten werden sortiert
-with open('bekannt.txt') as f:
-    alles = f.read()
-    f.close()
-    alles = alles.split()
-for i in range(len(alles)):
-    alles[i] = int(alles[i])
+    # alle Daten werden sortiert
+    with open('bekannt.txt') as f:
+        alles = f.read()
+        f.close()
+        alles = alles.split()
+    for i in range(len(alles)):
+        alles[i] = int(alles[i])
 
-# jetzt sind alle Daten unsortiert in alles
+    # jetzt sind alle Daten unsortiert in alles
+    for i in range(len(alles)):
+        buffer = i % 4
+        if buffer == 0:
+            a.append(alles[i])
+        elif buffer == 1:
+            b.append(alles[i])
+        elif buffer == 2:
+            c.append(alles[i])
+        else:
+            z.append(alles[i])
+    while len(alles) != 0:
+        alles.remove(alles[0])
+    # jetzt sind alle Daten in a, b, c, z aufgeteilt und damit verarbeitbar
 
-for i in range(len(alles)):
-    buffer = i % 4
-    if buffer == 0:
-        a.append(alles[i])
-    elif buffer == 1:
-        b.append(alles[i])
-    elif buffer == 2:
-        c.append(alles[i])
+    # die Testteile werden sortiert
+    with open('unbekannt.txt') as f:
+        alles = f.read()
+        f.close()
+        alles = alles.split()
+    for i in range(len(alles)):
+        alles[i] = int(alles[i])
+
+    # jetzt sind alle Daten unsortiert in alles
+
+    for i in range(len(alles)):
+        buffer = i % 4
+        if buffer == 0:
+            ta.append(alles[i])
+        elif buffer == 1:
+            tb.append(alles[i])
+        elif buffer == 2:
+            tc.append(alles[i])
+        else:
+            tz.append(alles[i])
+    while len(alles) != 0:
+        alles.remove(alles[0])
+    # jetzt sind die Punkte die geprüft werden sollen fertig sortiert in ta, tb, tc, tz
+
+    distanz = {}
+    # speichert den Abstand zwischen einem gegebenen Punkt (der der geprüft)
+    # und allen Punkten aus den Trainingsdaten (Postion = Nummer von Punkt)
+    # der Key ist Abstand und der Wert ist der Wert von dem Punkt
+
+    for i in range(len(a)):
+        buffer = wurzel(((a[i] - ta[x]) ** 2) + ((b[i] - tb[x]) ** 2) + ((c[i] - tc[x]) ** 2))
+        distanz[i] = [buffer, z[i]]
+
+    for i in range(len(distanz)):
+        if distanz[i][1] == 0:
+            result -= 1/distanz[i][0]
+        else:
+            result += 1/distanz[i][0]
+    # return result
+
+
+for i in range(5):
+    knn_gewichtet(i)
+    print('Der gegebene Punkt ist Punkt Nummer: ' + str(i))
+    print(ta[i], tb[i], tc[i])
+
+    if result == 0:
+        print('Kein Eindeutiges Ergebnis konnte ermittelt werden')
+    elif result > 0:
+        print('Der Gegebene Punkt ist Teil der Klassifikation. Die Sicherheit, dass das wirklich der Fall ist beträgt: ')
+        print(result)
     else:
-        z.append(alles[i])
-while len(alles) != 0:
-    alles.remove(alles[0])
-# jetzt sind alle Daten in a, b, c, z aufgeteilt und damit verarbeitbar
-
-
-# die Testteile werden sortiert
-with open('unbekannt.txt') as f:
-    alles = f.read()
-    f.close()
-    alles = alles.split()
-for i in range(len(alles)):
-    alles[i] = int(alles[i])
-
-# jetzt sind alle Daten unsortiert in alles
-
-for i in range(len(alles)):
-    buffer = i % 4
-    if buffer == 0:
-        ta.append(alles[i])
-    elif buffer == 1:
-        tb.append(alles[i])
-    elif buffer == 2:
-        tc.append(alles[i])
-    else:
-        tz.append(alles[i])
-while len(alles) != 0:
-    alles.remove(alles[0])
-# jetzt sind die Punkte die geprüft werden sollen fertig sortiert in ta, tb, tc, tz
-
-
-distanz = {}
-# speichert den Abstand zwischen einem gegebenen Punkt (der der geprüft)
-# und allen Punkten aus den Trainingsdaten (Postion = Nummer von Punkt)
-# der Key ist Abstand und der Wert ist der Wert von dem Punkt
-
-for i in range(len(a)):
-    buffer = wurzel(((a[i] - ta[x]) ** 2) + ((b[i] - tb[x]) ** 2) + ((c[i] - tc[x]) ** 2))
-    print(buffer)
-    distanz[buffer] = {z[i]}
-
-distanz2 = SortedDict(distanz)
-
-print(distanz)
-print(distanz2)
+        print('Der gegebene Punkt ist nicht Teil der Klassifikation. Die Sicherheit, dass das wirklich der Fall ist beträgt: ')
+        print(abs(result))
+    print()
