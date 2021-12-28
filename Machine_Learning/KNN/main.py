0e947bafade5d3e69be8bc26b3e9a7a627765ad2
BigSmoke908 <74255728+BigSmoke908@users.noreply.github.com>
2021-12-28 14:58:38 +0100

    neue Features
    - neues Feature: wenn ein Punkt bereits so in den Daten vorhanden ist, dann wird das so ausgegeben
    - Sonderfall, dass ein Punkt in den Daten bereits vorhanden ist führt nicht mehr länger zu einem Absturz von dem Programm

--- Machine_Learning/KNN/main.py
+++ Machine_Learning/KNN/main.py
@@ -1,19 +1,18 @@
 from math import sqrt as wurzel
 
+
 def knn_gewichtet(x):
-    global a, b, c, z, ta, tb, tc, tz, result
+    global a, b, c, z, ta, tb, tc, tz, result, in_Daten
     a = []
     b = []
     c = []
     z = []
-
     ta = []
     tb = []
     tc = []
     tz = []
-
+    in_Daten = False
     result = 0
-
 
     # alle Daten werden sortiert
     with open('bekannt.txt') as f:
@@ -38,16 +37,14 @@ def knn_gewichtet(x):
         alles.remove(alles[0])
     # jetzt sind alle Daten in a, b, c, z aufgeteilt und damit verarbeitbar
 
-    # die Testteile werden sortiert
+    # die Testdaten werden sortiert
     with open('unbekannt.txt') as f:
         alles = f.read()
         f.close()
         alles = alles.split()
     for i in range(len(alles)):
         alles[i] = int(alles[i])
-
     # jetzt sind alle Daten unsortiert in alles
-
     for i in range(len(alles)):
         buffer = i % 4
         if buffer == 0:
@@ -65,31 +62,47 @@ def knn_gewichtet(x):
     distanz = {}
     # speichert den Abstand zwischen einem gegebenen Punkt (der der geprüft)
     # und allen Punkten aus den Trainingsdaten (Postion = Nummer von Punkt)
-    # der Key ist Abstand und der Wert ist der Wert von dem Punkt
+    # die Keys werden nach den Nummern der Punkte in den Trainingsdaten einfach durchnummeriert
+    # (erst steht der Abstand, dann ob Teil der Kategorie oder nicht
 
     for i in range(len(a)):
-        buffer = wurzel(((a[i] - ta[x]) ** 2) + ((b[i] - tb[x]) ** 2) + ((c[i] - tc[x]) ** 2))
+        buffer = ((a[i] - ta[x]) ** 2) + ((b[i] - tb[x]) ** 2) + ((c[i] - tc[x]) ** 2)
+        if buffer != 0:
+            buffer = wurzel(buffer)
         distanz[i] = [buffer, z[i]]
 
     for i in range(len(distanz)):
-        if distanz[i][1] == 0:
+        if distanz[i][1] == 0 and distanz[i][0] != 0:
             result -= 1/distanz[i][0]
-        else:
+        elif distanz[i][0] != 0:
             result += 1/distanz[i][0]
-    # return result
+        else:
+            if distanz[i][1] == 0:
+                result = -1
+            else:
+                result = 1
+            in_Daten = True
+            break
 
 
-for i in range(5):
+for i in range(7):
     knn_gewichtet(i)
-    print('Der gegebene Punkt ist Punkt Nummer: ' + str(i))
-    print(ta[i], tb[i], tc[i])
+    print('Punkt Nummer: ' + str(i))
+    print('Eigenschaften:', ta[i], tb[i], tc[i])
 
-    if result == 0:
-        print('Kein Eindeutiges Ergebnis konnte ermittelt werden')
-    elif result > 0:
-        print('Der Gegebene Punkt ist Teil der Klassifikation. Die Sicherheit, dass das wirklich der Fall ist beträgt: ')
-        print(result)
+    if not in_Daten:
+        if result == 0:
+            print('Kein Eindeutiges Ergebnis konnte ermittelt werden')
+        elif result > 0:
+            print('Der Gegebene Punkt ist Teil der Klasse. Berechneter Wert:')
+            print(result)
+        else:
+            print('Der gegebene Punkt ist nicht Teil der Klasse. Berechneter Wert:')
+            print(abs(result))
     else:
-        print('Der gegebene Punkt ist nicht Teil der Klassifikation. Die Sicherheit, dass das wirklich der Fall ist beträgt: ')
-        print(abs(result))
+        print('Der Punkt ist so in den Trainingsdaten vorhanden.')
+        if result < 0:
+            print('In den Daten ist dieser Punkt nicht Teil der Klasse')
+        else:
+            print('In den Daten ist dieser Punkt Teil der Klasse')
     print()
