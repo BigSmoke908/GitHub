50c8b6a7ea1dde417b4f493be7983dc024dd43c1
BigSmoke908 <74255728+BigSmoke908@users.noreply.github.com>
2021-12-29 14:30:10 +0100

    Neue Datei make_daten.py
    es gibt eine Datei, mit der einfach Daten mit beliebigen Parametern erstellt werden können (kann wohlmöglich auch für andere Algorythmen hilfreich sein).
    
    Die anderen Dateien sind dem entsprechend (wegen neue Daten) ebenfalls verändert worden

---
+++ Machine_Learning/KNN/make_daten.py
@@ -0,0 +1,43 @@
+import random
+
+a = 10, 50
+# Bereich für Parameter a, in dem er richtig ist (beide Zahlen sind immer ausgeschlossen)
+b = 25, 30
+# Bereich für Parameter b
+c = 2, 10
+# Bereich für Parameter c
+x = 20
+# wie viele wahre Datenpunkte gegeben sein sollen
+y = 20
+# wie viele falsche Datenpunkte gegeben sein sollen
+
+wahr = 1
+falsch = 0
+# wie die einzelnen Datenpunkte wenn sie wahr/falsch sein gelabelt werden sollen (wird halbiert, eine Hälfte größer, andere kleiner)
+maxi = 60
+# wie groß eine maximale Zahl sein kann, für einen Datenpunkt der nicht der Klasse angehört
+
+with open('bekannt.txt', 'w') as f:
+    for i in range(x):
+        an = random.randrange(a[0] + 1, a[1] + 1, 1)
+        bn = random.randrange(b[0] + 1, b[1] + 1, 1)
+        cn = random.randrange(c[0] + 1, c[1] + 1, 1)
+        buffer = str(an) +  " " + str(bn) + " " + str(cn) + " 1"
+        print(buffer + '\n')
+        f.write(buffer + '\n')
+
+    not_Found = True
+
+    for i in range(y):
+        while not_Found:
+            an = random.randrange(0,  maxi, 1)
+            bn = random.randrange(0, maxi, 1)
+            cn = random.randrange(0, maxi, 1)
+
+            if an <= a[0] or an >= a[1] or bn <= b[0] or bn >= b[1] or cn <= c[0] or cn >= c[1]:
+                not_Found = False
+        buffer = str(an) + " " + str(bn) + " " + str(cn) + " 0"
+        f.write(buffer + '\n')
+        not_Found = True
+
+    f.close()
