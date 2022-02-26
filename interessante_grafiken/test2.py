6dcc44dce80a5c1e4bc50f7583f8921893c4e2c7
BigSmoke908 <74255728+BigSmoke908@users.noreply.github.com>
2022-02-26 16:45:18 +0100

    neue Funktion zum generieren von Heigtmaps erstellt
    ich habe eine neue Funktion zum generieren von heightmaps begonnen, bei dieser soll Multiprocessing von anfang an mit integriert sein. Kann soweit aber nur glätten

--- interessante_grafiken/test2.py
+++ interessante_grafiken/test2.py
@@ -1,15 +1,43 @@
-old = [[i + j for i in range(3)] for j in range(3)]
-
-for i in old:
-    print(i)
-print('---------')
-
-old.insert(0, old[0].copy())
-
-new = old.copy()
-new[0].insert(0, 'X')
+import time
+from multiprocessing import Pool
+from functools import partial
 
 
-for i in new:
-    print(i)
+def intensive_calc(i, x):
+    neu = [j for j in range(x)]
+    pool = Pool()
 
+    funcs = partial(intensive_calc_neu, i=i)
+    resultat = pool.map(funcs, iterable=neu)
+
+    return resultat
+
+
+def intensive_calc_neu(i, x):
+    anfang = time.time()
+    while time.time() - 1 < anfang:
+        x *= x
+    return i
+
+
+if __name__ == '__main__':
+    begin = time.time()
+    numbers = [i for i in range(100)]
+    p = Pool()
+
+    func = partial(intensive_calc, x=2)
+    result = p.map(func, iterable=numbers)
+    print(result)
+
+    p.close()
+    p.join()
+
+    print('Rechenzeit = ' + str(time.time() - begin))
+
+    begin = time.time()
+
+    result = []
+    for i in numbers:
+        result.append(intensive_calc(i, 2))
+    print(result)
+    print('Rechenzeit = ' + str(time.time() - begin))
